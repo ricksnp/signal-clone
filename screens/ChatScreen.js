263451710e581f5501ad6d477f39d42b6d513c06
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2021-02-04 18:03:33 -0500

    Added ability to add messages, also the method to fetch the chats

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,48 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar } from 'react-native-elements'
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons"
+import { SafeAreaView } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { KeyboardAvoidingView } from 'react-native'
+import { Platform } from 'react-native'
+import { ScrollView } from 'react-native'
+import { TextInput } from 'react-native'
+import { Keyboard } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native'
+import { db, auth } from '../firebase'
+import * as firebase from "firebase"
 
 // We also get route as a parameter, which gives us access to props along the way
 const ChatScreen = ({ navigation, route }) => {
+
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    const sendMessage = () => {
+        Keyboard.dismiss();
+        db.collection('chats').doc(route.params.id)
+            .collection('messages').add({
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                message: input,
+                displayName: auth.currentUser.displayName,
+                email: auth.currentUser.email,
+                photoURL: auth.currentUser.photoURL
+            })
+        setInput("");
+    }
+
+    useLayoutEffect(() => {
+        const unsubscribe = db.collection('chats').doc(route.params.id)
+            .collection('messages').orderBy('timestamp', "desc").onSnapshot((snapshot) => setMessages(
+                snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                }))
+            ))
+        return unsubscribe;
+    }, [route])
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -19,19 +56,77 @@ const ChatScreen = ({ navigation, route }) => {
                 </View>
             ),
             headerLeft: () => (
-                <TouchableOpacity>
+                <TouchableOpacity style={{ marginLeft: 10 }} onPress={navigation.goBack}>
                     <AntDesign name="arrowleft" size={24} color="white" />
                 </TouchableOpacity>
+            ),
+            headerRight: () => (
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: 80,
+                    marginRight: 20
+                }}>
+                    <TouchableOpacity>
+                        <FontAwesome name="video-camera" size={24} color="white" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="call" size={24} color="white" />
+                    </TouchableOpacity>
+                </View>
             )
         })
     }, [navigation])
     return (
-        <View>
-            <Text>I am Chat Screen</Text>
-        </View>
+        <SafeAreaView style={{
+            flex: 1, backgroundColor: "white"
+        }}>
+            <StatusBar style="light" />
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.container}
+                keyboardVerticalOffset={90}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <>
+                        <ScrollView>
+                            {/* Chat */}
+                        </ScrollView>
+                        <View style={styles.footer}>
+                            <TextInput placeholder="Signal Message" value={input} style={styles.textInput} onChangeText={text => setInput(text)} onSubmitEditing={sendMessage} />
+                            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+                                <Ionicons name="send" size={24} color="#2B68E6" />
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    footer: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: "100%",
+        padding: 15
+    },
+    textInput: {
+        // stick to bottom
+        bottom: 0,
+        height: 40,
+        // take up as much room as you can horizonally
+        flex: 1,
+        marginRight: 15,
+        backgroundColor: "#ECECEC",
+        padding: 10,
+        color: "grey",
+        borderRadius: 30
+    },
+})
