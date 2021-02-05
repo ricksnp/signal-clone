aa27b3e26f28ce159ac1c375a0276b5c370faf7a
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2021-02-05 17:46:56 -0500

    Reformatting / Added Chat functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,93 +1,95 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react'
-import { SafeAreaView, TouchableOpacity } from 'react-native'
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import { Avatar } from 'react-native-elements'
-import CustomListItem from '../components/CustomListItem'
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import CustomListItem from '../components/CustomListItem';
 import { auth, db } from '../firebase';
-import { AntDesign, SimpleLineIcons } from "@expo/vector-icons"
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons';
 
 const HomeScreen = ({ navigation }) => {
+	const [chats, setChats] = useState([]);
 
-    const [chats, setChats] = useState([]);
+	const signOutUser = () => {
+		auth.signOut().then(() => {
+			navigation.replace('Login');
+		});
+	};
 
-    const signOutUser = () => {
-        auth.signOut().then(() => {
-            navigation.replace('Login');
-        })
-    }
+	useEffect(() => {
+		const unsubscribe = db.collection('chats').onSnapshot((snapshot) =>
+			setChats(
+				snapshot.docs.map((doc) => ({
+					id: doc.id,
+					data: doc.data(),
+				}))
+			)
+		);
+		return unsubscribe;
+	}, []);
 
-    useEffect(() => {
-        const unsubscribe = db.collection('chats').onSnapshot(snapshot => (
-            setChats(snapshot.docs.map(doc => ({
-                id: doc.id,
-                data: doc.data()
-            })))
-        ))
-        return unsubscribe;
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			title: 'Signal',
+			headerStyle: { backgroundColor: '#fff' },
+			// Title of Header color
+			headerTitleStyle: { color: 'black' },
+			// Any icons in the header are black
+			headerTintColor: 'black',
+			headerLeft: () => (
+				<View style={{ marginLeft: 20 }}>
+					<TouchableOpacity activeOpacity={0.5} onPress={signOutUser}>
+						<Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+					</TouchableOpacity>
+				</View>
+			),
+			headerRight: () => (
+				<View
+					style={{
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						width: 80,
+						marginRight: 20,
+					}}
+				>
+					<TouchableOpacity activeOpacity={0.5}>
+						<AntDesign name="camerao" size={24} color="black" />
+					</TouchableOpacity>
 
-    }, [])
+					<TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate('AddChat')}>
+						<SimpleLineIcons name="pencil" size={24} color="black" />
+					</TouchableOpacity>
+				</View>
+			),
+		});
+	}, [navigation]);
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            title: "Signal",
-            headerStyle: { backgroundColor: "#fff" },
-            // Title of Header color
-            headerTitleStyle: { color: "black" },
-            // Any icons in the header are black
-            headerTintColor: "black",
-            headerLeft: () => (
-                <View style={{ marginLeft: 20 }}>
-                    <TouchableOpacity activeOpacity={0.5} onPress={signOutUser}>
-                        <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
-                    </TouchableOpacity>
-                </View>
-            ),
-            headerRight: () => (
-                <View style={{
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    width: 80,
-                    marginRight: 20
-                }}>
-                    <TouchableOpacity activeOpacity={0.5}>
-                        <AntDesign name='camerao' size={24} color="black" />
-                    </TouchableOpacity>
+	const enterChat = (id, chatName) => {
+		navigation.navigate('Chat', {
+			id,
+			chatName,
+		});
+	};
 
-                    <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate("AddChat")}>
-                        <SimpleLineIcons name="pencil" size={24} color="black" />
-                    </TouchableOpacity>
-                </View>
-            )
-        })
-    }, [navigation]);
+	return (
+		// SafeAreaView, in reference to an iPhone for example, makes
+		// sure that no areas are cut off due to the border / shape of
+		// an iphone
+		<SafeAreaView>
+			{/* Scrollable view */}
+			<ScrollView style={styles.container}>
+				{/* Remember to always have a key when rendering out via map */}
+				{chats.map(({ id, data: { chatName } }) => (
+					<CustomListItem key={id} id={id} chatName={chatName} enterChat={enterChat} />
+				))}
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
 
-    const enterChat = (id, chatName) => {
-        navigation.navigate('Chat', {
-            id,
-            chatName
-        })
-    }
-
-    return (
-        // SafeAreaView, in reference to an iPhone for example, makes
-        // sure that no areas are cut off due to the border / shape of
-        // an iphone
-        <SafeAreaView>
-            {/* Scrollable view */}
-            <ScrollView style={styles.container}>
-                {/* Remember to always have a key when rendering out via map */}
-                {chats.map(({ id, data: { chatName } }) => (
-                    <CustomListItem key={id} id={id} chatName={chatName} enterChat={enterChat} />
-                ))}
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
-
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        height: "100%"
-    }
-})
+	container: {
+		height: '100%',
+	},
+});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,69 +1,94 @@
-import { StatusBar } from 'expo-status-bar'
-import React, { useEffect, useState } from 'react'
-import { KeyboardAvoidingView } from 'react-native'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button, Input, Image } from "react-native-elements"
-import { auth } from '../firebase'
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, Input, Image } from 'react-native-elements';
+import { auth } from '../firebase';
 
 const LoginScreen = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    // Only run it on the component mounting
-    useEffect(() => {
-        //Essentially a real-time listener that is based on the status
-        //of a logged in or unlogged in user.  AKA, router guard if
-        // you're using react-router
-        const unsubscribe = auth.onAuthStateChanged((authUser) => {
-            if (authUser) {
-                navigation.replace('Home');
-            }
-        })
+	// Only run it on the component mounting
+	useEffect(() => {
+		//Essentially a real-time listener that is based on the status
+		//of a logged in or unlogged in user.  AKA, router guard if
+		// you're using react-router
+		const unsubscribe = auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				navigation.replace('Home');
+			}
+		});
 
-        // Will allow us to unsubscribe if the component re-mounts
-        return unsubscribe;
-    }, [])
+		// Will allow us to unsubscribe if the component re-mounts
+		return unsubscribe;
+	}, []);
 
-    const signIn = () => {
-        auth.signInWithEmailAndPassword(email, password).catch(error => alert(error));
-    }
-    return (
-        <KeyboardAvoidingView behavior='padding' style={styles.container}>
-            <StatusBar style="light" />
-            <Image source={{
-                uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
-            }}
-                style={{ width: 200, height: 200 }}
-            />
-            <View style={styles.inputContainer}>
-                <Input placeholder="Email" autoFocus type="email" value={email} onChangeText={text => setEmail(text)} />
-                <Input placeholder="Password" onSubmitEditing={signIn} secureTextEntry type="password" value={password} onChangeText={text => setPassword(text)} />
-            </View>
-            <Button containerStyle={styles.button} onPress={signIn} title="Login" />
-            <Button containerStyle={styles.button} onPress={() => navigation.navigate('Register')} type="outline" title="Register" />
-            {/* This view adds extra space so that when we go to type, the view is pushed up more */}
-            <View style={{ height: 100 }} />
-        </KeyboardAvoidingView>
-    )
-}
+	const signIn = () => {
+		auth
+			.signInWithEmailAndPassword(email, password)
+			.catch((error) => alert(error));
+	};
+	return (
+		<KeyboardAvoidingView behavior="padding" style={styles.container}>
+			<StatusBar style="light" />
+			<Image
+				source={{
+					uri:
+						'https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png',
+				}}
+				style={{ width: 200, height: 200 }}
+			/>
+			<View style={styles.inputContainer}>
+				<Input
+					placeholder="Email"
+					autoFocus
+					type="email"
+					value={email}
+					onChangeText={(text) => setEmail(text)}
+				/>
+				<Input
+					placeholder="Password"
+					onSubmitEditing={signIn}
+					secureTextEntry
+					type="password"
+					value={password}
+					onChangeText={(text) => setPassword(text)}
+				/>
+			</View>
+			<Button
+				containerStyle={styles.button}
+				onPress={signIn}
+				title="Login"
+			/>
+			<Button
+				containerStyle={styles.button}
+				onPress={() => navigation.navigate('Register')}
+				type="outline"
+				title="Register"
+			/>
+			{/* This view adds extra space so that when we go to type, the view is pushed up more */}
+			<View style={{ height: 100 }} />
+		</KeyboardAvoidingView>
+	);
+};
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        width: 300,
-    },
-    button: {
-        width: 200,
-        marginTop: 10
-    },
-    container: {
-        // Use the entire height
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: "center",
-        padding: 10,
-        backgroundColor: "white"
-
-    }
-})
+	inputContainer: {
+		width: 300,
+	},
+	button: {
+		width: 200,
+		marginTop: 10,
+	},
+	container: {
+		// Use the entire height
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 10,
+		backgroundColor: 'white',
+	},
+});
